b290ba61d413d0157eb7d9f91e1f09e5b00d0b9d
Sad Pencil <qh06@qq.com>
2022-02-17 00:07:56 +0800

    Update new-vhdx.ps1

--- Scripts/new-vhdx.ps1
+++ Scripts/new-vhdx.ps1
@@ -1,14 +1,14 @@
-﻿Param
+Param
 (
-        [Parameter(Mandatory = $true)]
-        [string]
-        $vhdxPath,
+    [Parameter(Mandatory = $true)]
+    [string]
+    $vhdxPath,
 
-        [int64]
-        $vhdxSize = 128GB
+    [int64]
+    $vhdxSize = 128GB
 )
 $ErrorActionPreference = "Stop"
 
+
 $vhd = New-VHD -Path $vhdxpath -Dynamic -SizeBytes $vhdxsize | Mount-VHD -Passthru
 $vhd | Initialize-Disk -Passthru | New-Partition -UseMaximumSize | Format-Volume -FileSystem NTFS -Confirm:$false -Force
-$vhd | Dismount-VHD
