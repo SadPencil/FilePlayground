b9d355349b6e36a0b62b62c109e639086ec4560b
Sad Pencil <qh06@qq.com>
2022-02-17 00:09:39 +0800

    Update mount-vhdx.ps1

--- Scripts/mount-vhdx.ps1
+++ Scripts/mount-vhdx.ps1
@@ -11,6 +11,7 @@ Param
 $ErrorActionPreference = "Stop"
 
 
+New-Item $mountPath -ItemType Directory -ErrorAction SilentlyContinue
 $vhd = Mount-VHD -NoDriveLetter -Path $vhdChildPath -PassThru
 $part = $vhd | Get-Disk | Get-Partition | Where-Object { ($_ | Get-Volume) -ne $Null } 
 $part | Add-PartitionAccessPath -AccessPath $mountPath
