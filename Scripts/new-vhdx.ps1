01039c08c6d5a89dfe5ce293c4cb9e9fe4194902
Sad Pencil <qh06@qq.com>
2022-02-16 23:44:29 +0800

    Create new-vhdx.ps1

---
+++ Scripts/new-vhdx.ps1
@@ -0,0 +1,14 @@
+﻿Param
+(
+        [Parameter(Mandatory = $true)]
+        [string]
+        $vhdxPath,
+
+        [int64]
+        $vhdxSize = 128GB
+)
+$ErrorActionPreference = "Stop"
+
+$vhd = New-VHD -Path $vhdxpath -Dynamic -SizeBytes $vhdxsize | Mount-VHD -Passthru
+$vhd | Initialize-Disk -Passthru | New-Partition -UseMaximumSize | Format-Volume -FileSystem NTFS -Confirm:$false -Force
+$vhd | Dismount-VHD
